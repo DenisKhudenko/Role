8e3d83611d608fc7f2c5573b7a1c50e5307a0512
Denis Khudenko <89531592151.denis@gmail.com>
2026-06-01 11:46:48 +0300

    git init

---
+++ RoleWeb/Program.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Role",
+        Version = "v1"
+    });
+
+    // Summary � UI Swagger
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    
+    if (File.Exists(xmlPath))
+    {
+        swagger.IncludeXmlComments(xmlPath);
+    }
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(swagger =>
+    {
+        swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "Role v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
